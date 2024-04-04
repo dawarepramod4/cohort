fe5c98c1ac4a10777cb8a89efd0fb62dd22018ad
Pramod Daware <dawarepramod4@gmail.com>
2024-04-05 00:50:08 +0530

    first commit

---
+++ 8_tailwind/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="flex justify-between hover:flex-col">
+                <div>dfdds</div>
+                <div>dfdds</div>
+                <div>dfdds</div>
+                <div>dfdds</div>
+                <div>dfdds</div>
+            </div>
+        </>
+    );
+}
+
+export default App;
