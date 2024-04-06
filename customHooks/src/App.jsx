044e7d10a5f5eca141147fc6f775edb99e08a280
Pramod Daware <dawarepramod4@gmail.com>
2024-04-07 01:00:42 +0530

    isonline hooks

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,55 +1,28 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import swrfetch from "swr";
-import useSWR from "swr";
-//custom hook
-// function useTodos(n) {
-//     const [todos, setTodos] = useState([]);
-//     const [loading, setLoading] = useState(true);
-//     useEffect(() => {
-//         const value = setInterval(() => {
-//             axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-//                 setTodos(res.data.todos);
-//                 setLoading(false);
-//             });
-//         }, 5000);
+import "./App.css";
+import React from "react";
 
-//         axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-//             setTodos(res.data.todos);
-//             setLoading(false);
-//         });
-
-//         //cleanup
-//         return () => {
-//             clearInterval(value);
-//         };
-//     }, [n]);
-
-//     return [todos, loading];
-// }
-
-async function fetcher(url) {
-    const data = await fetch(url);
-    const json1 = await data.json();
-    console.log(json1.todos);
-    return json1.todos;
+function isOnline() {
+    return navigator.onLine;
 }
 
-function App() {
-    // const [todos, loading] = useTodos(5);
-    const { todos, error, loading } = useSWR("https://sum-server.100xdevs.com/todos", fetcher);
-
-    return <>{!loading ? <></> : <div>sdf dfdf dsfd fdsf s fsd fsfsd fsdf s fsLoading...</div>}</>;
+function useIsOnline() {
+    const [online, setOnline] = React.useState(isOnline());
+    React.useEffect(() => {
+        const handler = () => setOnline(isOnline());
+        window.addEventListener("online", handler);
+        window.addEventListener("offline", handler);
+        return () => {
+            window.removeEventListener("online", handler);
+            window.removeEventListener("offline", handler);
+        };
+    }, []);
+    return online;
 }
 
-function Track({ todo }) {
-    return (
-        <div>
-            {todo.title}
-            <br />
-            {todo.description}
-        </div>
-    );
-}
+export default function App() {
+    const online = useIsOnline();
+    if (online) console.log("Online");
+    else console.log("Offline");
 
-export default App;
+    return <h1>am i Online : {online == true ? <>true</> : <>false</>}</h1>;
+}
