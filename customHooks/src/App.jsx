19cf41359207bc47d89353fadd78263f892710f0
Pramod Daware <dawarepramod4@gmail.com>
2024-04-07 00:48:47 +0530

    useswr

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,36 +1,45 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import swrfetch from "swr";
+import useSWR from "swr";
 //custom hook
-function useTodos(n) {
-    const [todos, setTodos] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const value = setInterval(() => {
-            axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-                setTodos(res.data.todos);
-                setLoading(false);
-            });
-        }, 5000);
+// function useTodos(n) {
+//     const [todos, setTodos] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//     useEffect(() => {
+//         const value = setInterval(() => {
+//             axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+//                 setTodos(res.data.todos);
+//                 setLoading(false);
+//             });
+//         }, 5000);
 
-        axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
-            setTodos(res.data.todos);
-            setLoading(false);
-        });
+//         axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+//             setTodos(res.data.todos);
+//             setLoading(false);
+//         });
 
-        //cleanup
-        return () => {
-            clearInterval(value);
-        };
-    }, [n]);
+//         //cleanup
+//         return () => {
+//             clearInterval(value);
+//         };
+//     }, [n]);
 
-    return [todos, loading];
+//     return [todos, loading];
+// }
+
+async function fetcher(url) {
+    const data = await fetch(url);
+    const json1 = await data.json();
+    console.log(json1.todos);
+    return json1.todos;
 }
 
 function App() {
-    const [todos, loading] = useTodos(5);
+    // const [todos, loading] = useTodos(5);
+    const { todos, error, loading } = useSWR("https://sum-server.100xdevs.com/todos", fetcher);
 
-    return <>{!loading ? todos.map((todo) => <Track todo={todo} />) : <div>Loading...</div>}</>;
+    return <>{!loading ? <></> : <div>sdf dfdf dsfd fdsf s fsd fsfsd fsdf s fsLoading...</div>}</>;
 }
 
 function Track({ todo }) {
