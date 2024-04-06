68a830f1e2548b47f3fcd8f53fedd50f197511e9
Pramod Daware <dawarepramod4@gmail.com>
2024-04-07 00:24:19 +0530

    custom hooks usetodod with cleanup

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,26 +1,46 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import axios from "axios";
+
+//custom hook
+function useTodos(n) {
+    const [todos, setTodos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const value = setInterval(() => {
+            axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+                setTodos(res.data.todos);
+                setLoading(false);
+            });
+        }, 5000);
+
+        axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+            setTodos(res.data.todos);
+            setLoading(false);
+        });
+
+        //cleanup
+        return () => {
+            clearInterval(value);
+        };
+    }, [n]);
+
+    return [todos, loading];
+}
 
 function App() {
-    // const [count, setCount] = useState(0);
-    const [render, setRender] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setRender(false);
-        }, 3000);
-    });
-    return <>{render ? <Mycomponent /> : <div></div>}</>;
-}
-function Mycomponent() {
-    useEffect(() => {
-        console.log("Component Mounted");
-        return () => {
-            console.log("Component Unmounted");
-        };
-    });
+    const [todos, loading] = useTodos(5);
 
-    return <div>From inside the component</div>;
+    return <>{!loading ? todos.map((todo) => <Track todo={todo} />) : <div>Loading...</div>}</>;
 }
+
+function Track({ todo }) {
+    return (
+        <div>
+            {todo.title}
+            <br />
+            {todo.description}
+        </div>
+    );
+}
+
 export default App;
